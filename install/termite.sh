dc2fab96bacc23729d25ca5cd043386fe45542e4
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-18 00:27:11 -0600

    Create termite.sh

---
+++ install/termite.sh
@@ -0,0 +1,21 @@
+sudo apt-get install gtk-doc-tools libglib2.0-dev gobject-introspection valac libpango1.0-dev libgtk-3-dev \
+  libgnutls28-dev g++ libgirepository1.0-dev libxml2-utils gperf libpcre2-dev
+
+mkdir $HOME/termite-install
+cd $HOME/termite-install
+
+git clone --recursive https://github.com/thestinger/termite.git
+git clone https://github.com/thestinger/vte-ng.git
+
+mkdir devenv
+
+cd vte-ng && ./autogen.sh --prefix=$(realpath ../devenv) && make -j$(nproc) && make install
+
+cd ../termite
+sed -i 's/$</$< ..\/devenv\/lib\/libvte-2.91.a/' Makefile
+sed -i 's/${DESTDIR}\/etc/${DESTDIR}${PREFIX}\/etc/' Makefile
+PKG_CONFIG_PATH=$(realpath ../devenv/lib/pkgconfig) make -j$(nproc)
+
+sudo make -C termite/ install
+
+rm -rf $HOME/termite-install
