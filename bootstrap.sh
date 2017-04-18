1d6e6485d59126fb179b33aeb58845dff20e1dc7
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-17 23:59:05 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,8 +8,10 @@ sudo apt-get -y install virtualbox-guest-dkms
 # git and stow are used to clone the dotfiles repository and symlink configuration files
 sudo apt-get -y install git stow
 
-# Move existing dotfiles here before calling stow
+# Move existing config files that will be symlinked using stow to this folder
 mkdir $HOME/.olddotfiles
 
 git clone https://github.com/lloydk/dotfiles.git $HOME/.dotfiles
 
+# Allow changes to the repo to be pushed to github after ssh keys have been installed
+git remote set-url origin git@github.com:lloydk/dotfiles.git
