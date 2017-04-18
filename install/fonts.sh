8ef20813cca005d733ad5d70e8ebd0beec33c210
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-18 01:11:33 -0600

    Update fonts.sh

--- install/fonts.sh
+++ install/fonts.sh
@@ -1,14 +1,13 @@
+# Adobe Source Code Pro
 cd $HOME/Downloads
+rm 1.050R-it.zip
+rm -rf source-code-pro-2.030R-ro-1.050R-it
 wget https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
-
 unzip 1.050R-it.zip
-
 mkdir -p ~/.fonts
-
 cp source-code-pro-2.030R-ro-1.050R-it/TTF/*.ttf ~/.fonts/
-
 sudo fc-cache -f -v
-
 rm 1.050R-it.zip
 rm -rf source-code-pro-2.030R-ro-1.050R-it
 
+# Roboto
