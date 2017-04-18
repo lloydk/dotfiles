a2cc6f0698471bc30d93d102a8050a8d014a1ee7
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-17 23:30:02 -0600

    Create ssh_keys.sh

---
+++ install/ssh_keys.sh
@@ -0,0 +1,6 @@
+cp -r /media/lloydk/ESD-USB/.ssh $HOME
+chmod 700 $HOME/.ssh
+chmod 600 $HOME/.ssh/id_rsa
+chmod 644 $HOME/.ssh/id_rsa.pub
+chmod 600 $HOME/.ssh/config
+#chmod 644 $HOME/.ssh/authorized_keys
