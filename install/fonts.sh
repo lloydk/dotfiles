06cb1d04f83e1adfe263021c4b1a9ef00da276c5
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-18 01:01:53 -0600

    Create fonts.sh

---
+++ install/fonts.sh
@@ -0,0 +1,14 @@
+cd $HOME/Downloads
+wget https://github.com/adobe-fonts/source-code-pro/archive/2.030R-ro/1.050R-it.zip
+
+#unzip SourceCodePro_FontsOnly-$version.zip
+#mkdir -p ~/.fonts
+
+#cp SourceCodePro_FontsOnly-$version/OTF/*.otf ~/.fonts/
+
+#echo "\n* Updating font cache"
+#sudo fc-cache -f -v
+
+#echo "\n* Looking for 'Source Code Pro' in installed fonts"
+#fc-list | grep "Source Code Pro"
+
