dda20b1b0c20d6f55e5528bd2741d46e7a0b7888
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-17 21:17:55 -0600

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,9 @@
+sudo apt-get update
+
+sudo apt-get -y upgrade
+
+# Needed for proper screen resolution and host OS integration
+sudo apt-get -y install virtualbox-guest-dkms
+
+# git and stow are used to clone the dotfiles repository and symlink configuration files
+sudo apt-get -y install git stow
