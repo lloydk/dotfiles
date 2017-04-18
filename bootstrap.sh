f22e8d84bea17f648b2b79d888c22a2e29c36f84
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-17 21:38:21 -0600

    Add cloning of dotfiles repository to bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,3 +7,8 @@ sudo apt-get -y install virtualbox-guest-dkms
 
 # git and stow are used to clone the dotfiles repository and symlink configuration files
 sudo apt-get -y install git stow
+
+cd $HOME
+
+git clone https://github.com/lloydk/dotfiles.git
+
