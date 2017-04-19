93b9b7126b18ef725d3be3573ce928b950b0c0a5
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-18 20:26:53 -0600

    Update termite.sh

--- install/termite.sh
+++ install/termite.sh
@@ -1,3 +1,5 @@
+# Based on https://github.com/xiouy/termite-install_ubuntu16.04.1
+
 sudo apt-get install -y gtk-doc-tools libglib2.0-dev gobject-introspection valac libpango1.0-dev libgtk-3-dev \
   libgnutls28-dev g++ libgirepository1.0-dev libxml2-utils gperf libpcre2-dev
 
@@ -19,5 +21,7 @@ PKG_CONFIG_PATH=$(realpath ../devenv/lib/pkgconfig) make -j$(nproc)
 cd $HOME/termite-install
 
 sudo make -C termite/ install
+
+sudo mkdir -p /lib/terminfo/x; sudo ln -s /usr/local/share/terminfo/x/xterm-termite /lib/terminfo/x/xterm-termite
 
 rm -rf $HOME/termite-install
