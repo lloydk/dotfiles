5c809b560ac59fb6814d4c85ac0c7851b160e0d5
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-21 01:23:45 -0600

    Update ruby.sh

--- install/ruby.sh
+++ install/ruby.sh
@@ -5,6 +5,7 @@ cd ruby-install-0.6.1/
 sudo make install
 cd ..
 rm -rf ruby-install-0.6.1
+rm ruby-install-0.6.1.tar.gz
 
 sudo ruby-install --system ruby 2.4.1
 
@@ -14,3 +15,4 @@ cd chruby-0.3.9/
 sudo make install
 cd ..
 rm -rf chruby-0.3.9
+rm chruby-0.3.9.tar.gz
