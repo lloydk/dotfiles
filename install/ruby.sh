9e81d64ed662da19e0c29942a21d1fdfce921a7c
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-21 01:33:29 -0600

    Update ruby.sh

--- install/ruby.sh
+++ install/ruby.sh
@@ -7,7 +7,7 @@ cd ..
 rm -rf ruby-install-0.6.1
 rm ruby-install-0.6.1.tar.gz
 
-sudo ruby-install --system ruby 2.4.1
+sudo ruby-install ruby 2.4.1
 
 wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
 tar -xzvf chruby-0.3.9.tar.gz
