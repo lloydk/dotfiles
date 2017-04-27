8cd2d89acf30068a4e7035dbc4f7ec458519f0be
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-27 15:09:53 -0600

    Ensure nodejs PGP key is added to main keyring

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,4 +1,5 @@
 # assumes asdf version manager has been installed
 
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
 asdf install nodejs 7.9.0
