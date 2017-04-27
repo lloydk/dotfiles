44868839e582b8d810a394383199a742a5117a42
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-27 01:29:34 -0600

    Update ruby.sh

--- install/ruby.sh
+++ install/ruby.sh
@@ -1,21 +1,4 @@
-cd ~/Downloads
-wget -O ruby-install-0.6.1.tar.gz https://github.com/postmodern/ruby-install/archive/v0.6.1.tar.gz
-tar -xzvf ruby-install-0.6.1.tar.gz
-cd ruby-install-0.6.1/
-sudo make install
-cd ..
-rm -rf ruby-install-0.6.1
-rm ruby-install-0.6.1.tar.gz
+# assumes asdf version manager plugin has been installed
 
-ruby-install ruby 2.4.1
-
-wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
-tar -xzvf chruby-0.3.9.tar.gz
-cd chruby-0.3.9/
-sudo make install
-cd ..
-rm -rf chruby-0.3.9
-rm chruby-0.3.9.tar.gz
-
-cd $HOME/.dotfiles
-stow ruby
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf install ruby 2.4.1
