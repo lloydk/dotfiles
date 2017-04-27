88e78c2d51fd7040ad623ac05a8edb0341f0e804
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-27 01:44:03 -0600

    Update i3.sh

--- install/i3.sh
+++ install/i3.sh
@@ -1,8 +1,10 @@
+cd ~/Downloads
 /usr/lib/apt/apt-helper download-file http://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2017.01.02_all.deb keyring.deb SHA256:4c3c6685b1181d83efe3a479c5ae38a2a44e23add55e16a328b8c8560bf05e5f
-apt install ./keyring.deb
-echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
-apt update
-apt install -y i3 i3blocks rofi
+sudo apt install ./keyring.deb
+echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | sudo cut -f2 -d=) universe" >> /etc/apt/sources.list.d/sur5r-i3.list
+sudo apt update
+sudo apt install -y i3 i3blocks rofi
 rm keyring.deb
+
 cd $HOME/.dotfiles
 stow i3
