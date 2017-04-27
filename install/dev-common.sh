9795aa6168207166a3e9580237c1d239d3e7f9cc
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-27 02:31:14 -0600

    Update dev-common.sh

--- install/dev-common.sh
+++ install/dev-common.sh
@@ -1,4 +1,4 @@
 sudo apt install -y vim curl
 
 # Common packages required by asdf plugins
-sudo apt-install -y automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
+sudo apt install -y automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev libxslt-dev libffi-dev libtool unixodbc-dev
