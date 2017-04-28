fb430c4cbf4b275f4f6d487b8a6fbcaf3cabd036
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-04-27 23:47:06 -0600

    Update erlang dependencies

--- install/erlang.sh
+++ install/erlang.sh
@@ -1,2 +1,4 @@
+sudo apt install -y build-essential autoconf m4 libncurses5-dev libwxbase3.0-dev libwxgtk3.0-dev libgl1-mesa-dev libglu1-mesa-dev libpng3 libssh-dev unixodbc-dev
+
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf install erlang 19.3
