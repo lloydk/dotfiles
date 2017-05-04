4163d8cb410ab2c9eb4dce23d8b337a713cd7f4c
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-05-03 21:28:59 -0600

    Install ansible from ppa

--- install/ansible.sh
+++ install/ansible.sh
@@ -1,8 +1,9 @@
 sudo apt install -y python-setuptools python-dev sshpass
+sudo apt install -y software-properties-common
+sudo apt-repository ppa:ansible/ansible
+sudo apt update
+sudo apt install -y ansible
 mkdir -p ~/work
 cd ~/work
-git clone git://github.com/ansible/ansible.git --recursive 
-sudo easy_install pip
-cd ~/work/ansible
-sudo pip install -r ./requirements.txt
+# sudo easy_install pip
 
