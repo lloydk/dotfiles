e1863ac940c3466090e4f5fe3ea1d9cf749e3571
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-05-03 21:50:49 -0600

    Fix error adding apt repository for ansible

--- install/ansible.sh
+++ install/ansible.sh
@@ -1,9 +1,6 @@
 sudo apt install -y python-setuptools python-dev sshpass
 sudo apt install -y software-properties-common
-sudo apt-repository ppa:ansible/ansible
+sudo add-apt-repository ppa:ansible/ansible
 sudo apt update
 sudo apt install -y ansible
-mkdir -p ~/work
-cd ~/work
-# sudo easy_install pip
 
