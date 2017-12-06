25454577a3a3d8fdfe60002c6ddad5c2e96ddd09
Lloyd Kupchanko <lloyd@lloydk.ca>
2017-12-06 03:43:55 -0700

    Upgrade to ruby 2.4.2

--- install/ruby.sh
+++ install/ruby.sh
@@ -1,6 +1,6 @@
 # assumes asdf version manager plugin has been installed
 
 asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
-asdf install ruby 2.4.1
+asdf install ruby 2.4.2
 cd $HOME/.dotfiles
 stow ruby
